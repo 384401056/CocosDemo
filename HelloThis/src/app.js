e03e7ffaaa4762d9ed0d2cbf5c1a2a96335eccc2
Gaoyanbin <384401056@qq.com>
2016-03-16 13:34:20 +0800

    HelloThis 完成

--- HelloThis/src/app.js
+++ HelloThis/src/app.js
@@ -1,44 +1,108 @@
-
+//菜单与文字层。.
 var HelloWorldLayer = cc.Layer.extend({
-    sprite:null,
-    ctor:function () {
-        //////////////////////////////
-        // 1. super init first
+    helloLabel:null,
+    ctor:function() {
         this._super();
+        var size = cc.director.getWinSize();
 
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
-        var size = cc.winSize;
+        //新建菜单项
+        var closeItem = new cc.MenuItemImage(
+            res.normal_png,
+            res.selected_png,
+            res.disabled_png,
+            this.closeClicked, //要绑定的事件.
+            this//事件的目标
+        );
 
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
-        // position the label on the center of the screen
-        helloLabel.x = size.width / 2;
-        helloLabel.y = size.height / 2 + 200;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
+        closeItem.anchorX = 0.5;
+        closeItem.anchorY = 0.5;
 
-        // add "HelloWorld" splash screen"
-        this.sprite = new cc.Sprite(res.HelloWorld_png);
-        this.sprite.attr({
-            x: size.width / 2,
-            y: size.height / 2
-        });
-        this.addChild(this.sprite, 0);
+        //菜单项加入菜单
+        var menu = new cc.Menu(closeItem);
+        menu.x = size.width/2-300;
+        menu.y = size.height/2+200;
+
+        //菜单加入Layer
+        this.addChild(menu,1);
+
+        this.helloLabel = new cc.LabelTTF("HelloLable","Arial",38);
+        this.helloLabel.x = size.width/2;
+        this.helloLabel.y = size.height/2;
+        this.addChild(this.helloLabel,5);
 
         return true;
+    },
+
+    //菜单项的点击事件.
+    closeClicked: function(){
+        this.removeChild(this.helloLabel);
     }
+});
+
+
+
+//动画层
+var animationLayer = cc.Layer.extend({
+
+    deltaX:2,
+    ball:null,
+    frame:0,
+    bg:null,
+    dotColor:null,
+
+    ctor:function(){
+        this._super();
+        var size = cc.director.getWinSize();
+
+
+
+        this.ball = new cc.Sprite(res.ball_png);
+        this.ball.x = 0;
+        this.ball.y = size.width/2;
+        this.addChild(this.ball,1);
+
+
+        this.bg = new cc.DrawNode();//用于记录球的运动轨迹。
+        this.addChild(this.bg,0);
+
+        this.scheduleUpdate();//通知当前节点，在每帧重绘之前调用update函数。
+
+        return true;
+    },
+
+    update: function(){
+
+        var size = cc.director.getWinSize();
+
+        //根据deltaX的正反方向来惊变颜色。
+        if(this.deltaX>0){
+            this.dotColor = new cc.color(255,204,0);
+        }else{
+            this.dotColor = new cc.color(51,153,255);
+        }
+
+
+        this.ball.x += this.deltaX;
+
+        //如果ball超出屏幕,改变增量的方向。
+        if(this.ball.x>=size.width || this.ball.x <=0){
+            this.deltaX *= -1;    //对增量取反1变为-1，-1变为1.
+        }
+
+        this.ball.y = Math.sin(this.frame/20)*50+size.height/2;
+
+        //drawDot画出点，参数为位置、点半径和颜色。
+        this.bg.drawDot(new cc.Point(this.ball.x,this.ball.y),2, this.dotColor);
+
+        this.frame++;
+    }
+
 });
 
 var HelloWorldScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-        var layer = new HelloWorldLayer();
+        var layer = new animationLayer();
         this.addChild(layer);
     }
 });
--- HelloThis/src/resource.js
+++ HelloThis/src/resource.js
@@ -1,5 +1,9 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
+    normal_png : "res/normal_png.png",
+    selected_png : "res/selected_png.png",
+    ball_png:"res/ball.png",
+    disabled_png : "res/disabled_png.png"
 };
 
 var g_resources = [];
