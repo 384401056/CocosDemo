afb39cda95739036522c55f0bdbb303a44cd8192
Gaoyanbin <384401056@qq.com>
2016-03-23 10:29:18 +0800

    GameUI

---
+++ GameUI/src/resource.js
@@ -0,0 +1,20 @@
+var res = {
+    start_normal : "res/start_normal.png",
+    start_press : "res/start_press.png",
+    start_dis : "res/start_dis.png",
+    HelloWorld_png : "res/HelloWorld.png",
+    MainScene:"res/res/MainScene.json",
+    LoadingBarFile:"res/res/Default/LoadingBarFile.png",
+    Slider_Back:"res/res/Default/Slider_Back.png",
+    Slider_PressBar:"res/res/Default/Slider_PressBar.png",
+    SliderNode_Disable:"res/res/Default/SliderNode_Disable.png",
+    SliderNode_Normal:"res/res/Default/SliderNode_Normal.png",
+    SliderNode_Press:"res/res/Default/SliderNode_Press.png"
+};
+
+var g_resources = [
+
+];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
